0c67f2cabc97dce75fec0e993b7ec3804ef6bc1a
jelenadjordjevic <lena_magdalena@live.com>
2019-03-07 11:30:53 +0100

    gfhgdgdgd

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './_components/login/login.component';
 import { RegisterComponent } from './_components/register/register.component';
+import { TestComponent } from './_components/test/test.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, TestComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
