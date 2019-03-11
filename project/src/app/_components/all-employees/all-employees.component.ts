c13793d287591413881e2f092606d5dcd842d43c
jelenadjordjevic <lena_magdalena@live.com>
2019-03-11 19:02:53 +0100

    ...

--- project/src/app/_components/all-employees/all-employees.component.ts
+++ project/src/app/_components/all-employees/all-employees.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { businessTripAnimation, employeeAnimation } from 'src/app/_animations/business-trip.animation';
 
 @Component({
   selector: 'app-all-employees',
   templateUrl: './all-employees.component.html',
-  styleUrls: ['./all-employees.component.scss']
+  styleUrls: ['./all-employees.component.scss'],
+  animations: [businessTripAnimation, employeeAnimation]
 })
 export class AllEmployeesComponent implements OnInit {
-
+  businessTripTrigger = 'open';
   constructor() { }
 
   ngOnInit() {
   }
-
+  onBusinessTrip() {
+    //this.businesTrip = this.employee.businessTrips[ind - 1];
+    //console.log(this.businesTrip);
+    this.businessTripTrigger = 'open';
+  }
+  onCloseBusinessTrip() {
+    this.businessTripTrigger = 'closed';
+  }
 }
